67b75004b51f13131dea937e1123c26fd9f0ebd6
Andrei Popovici <andreipopovici@pvh.com>
2022-05-03 03:02:36 +0100

    Mocking some holiday objects for unit tests

--- components/Holiday/__test__/Holidays.spec.tsx
+++ components/Holiday/__test__/Holidays.spec.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import { Mock } from 'ts-mockery';
 import { render } from '@testing-library/react';
 import Holidays from '../Holidays';
+import { Holiday as HolidayType } from '@lib/api/types';
 
 jest.mock('@components/Holiday', () => ({
   __esModule: true,
-  HolidayTile: (props): JSX.Element => <div data-testid="HolidayTile" {...props} />,
+  HolidayTile: (props: any): JSX.Element => <div data-testid="HolidayTile" {...props} />,
 }));
 
 describe('Holidays', () => {
   it('should render the Holidays', () => {
-    const { container } = render(<Holidays holidays={['holiday 1', 'holiday 2']} />);
+    const mockHolidays = [Mock.of<HolidayType>({}), Mock.of<HolidayType>({})];
+    const { container } = render(<Holidays holidays={mockHolidays} />);
     expect(container).toMatchSnapshot();
   });
   it('should render no holidays', () => {
-    const { container } = render(<Holidays />);
+    const { container } = render(<Holidays holidays={} />);
     expect(container).toMatchSnapshot();
   });
 });
